03403d346e16a5c47e2b2b636650496a042fcba1
Jonathan Lim <jl14274@my.bristol.ac.uk>
2021-01-23 12:15:33 +0000

    Added more templates

---
+++ src/templates/cmake.js
@@ -0,0 +1,18 @@
+const data = `
+# -----------------------
+# CMake
+# -----------------------
+CMakeLists.txt.user
+CMakeCache.txt
+CMakeFiles
+CMakeScripts
+Testing
+Makefile
+cmake_install.cmake
+install_manifest.txt
+compile_commands.json
+CTestTestfile.cmake
+_deps
+`;
+
+export default data;
--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,12 +1,27 @@
 import c_data from "./c";
+import cmake_data from "./cmake";
 import cpp_data from "./cpp";
-import python_data from "./python";
+import go_data from "./go";
+import java_data from "./java";
 import labview_data from "./labview";
+import node_data from "./node";
+import python_data from "./python";
+import r_data from "./r";
+import ruby_data from "./ruby";
+import rust_data from "./rust";
+
+// Templates obtained from https://github.com/github/gitignore
 
 export const initialTemplates = [
   { name: "c", data: c_data, checked: false },
+  { name: "cmake", data: cmake_data, checked: false },
   { name: "c++", data: cpp_data, checked: false },
-  { name: "altium", data: "Some data", checked: false },
-  { name: "python", data: python_data, checked: false },
+  { name: "go", data: go_data, checked: false },
+  { name: "java", data: java_data, checked: false },
   { name: "labview", data: labview_data, checked: false },
+  { name: "node", data: node_data, checked: false },
+  { name: "python", data: python_data, checked: false },
+  { name: "r", data: r_data, checked: false },
+  { name: "ruby", data: ruby_data, checked: false },
+  { name: "rust", data: rust_data, checked: false },
 ];
